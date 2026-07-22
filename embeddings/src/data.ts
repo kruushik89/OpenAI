f412b56c38fb249a7699b5de6590211c490355df
oleksandrkrushynskyi <kruushik89@gmail.com>
2026-07-22 11:51:38 +0300

    Implement data loading and saving functionality in embeddings module: add `loadJSONData` and `saveDataToJSONFile` utilities, enhance `generateEmbeddings` function, and create `main` function to process data from `data.json` and save results to `dataWithEmbeddings.json`.

--- embeddings/src/data.ts
+++ embeddings/src/data.ts
@@ -1,14 +1,48 @@
 import OpenAI from "openai";
+import { readFileSync, writeFileSync } from "fs";
+import { join } from "path";
 
 const openai = new OpenAI()
 
-async function generateEmbeddings(text: string) {
-    const response = await openai.embeddings.create({
-        model: "text-embedding-3-small",
-        input: text,
-    });
-    console.log("text: ", text, "embedding: ", response.data[0].embedding);
-    return response;
+type DataWithEmbeddings = {
+    input: string;
+    embedding: number[];
 }
 
-generateEmbeddings("cat");
+async function generateEmbeddings(input: string | string[]) {
+    const response = await openai.embeddings.create({
+        model: "text-embedding-3-small",
+        input,
+    });
+    return response.data;
+}
+
+function loadJSONData<T>(filePath: string): T {
+    const path = join(__dirname, filePath);
+    const rowData = readFileSync(path);
+    return JSON.parse(rowData.toString());
+}
+
+function saveDataToJSONFile(data: any, fileName: string) {
+    const dataString = JSON.stringify(data);
+    const dataBuffer = Buffer.from(dataString);
+    const path = join(__dirname, fileName);
+    writeFileSync(path, dataBuffer);
+    console.log(`Data saved to ${fileName}`);
+}
+
+async function main() {
+    const data = loadJSONData<string[]>("data.json");
+    const embeddings = await generateEmbeddings(data);
+    const dataWithEmbeddings: DataWithEmbeddings[] = []
+
+    for (let i = 0; i < data.length; i++) {
+        dataWithEmbeddings.push({
+            input: data[i],
+            embedding: embeddings[i].embedding,
+        });
+    }
+    saveDataToJSONFile(dataWithEmbeddings, "dataWithEmbeddings.json");
+}
+
+main();
